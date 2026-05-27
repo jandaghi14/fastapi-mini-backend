387f6b59b9ebcc2434c99151e2d57a718c7f6087
Ali Jandaghi <jandaghi14@gmail.com>
2026-05-27 13:13:55 +0330

    feat: add rate limiting to login endpoint and flake8 to CI pipeline

--- app/presentation/routers/user_router.py
+++ app/presentation/routers/user_router.py
@@ -1,10 +1,12 @@
-from fastapi import APIRouter
-from app.presentation import schemas
-from app.core import user_service
+from fastapi import APIRouter, Request
 from fastapi import Depends, HTTPException
 from sqlalchemy.orm import Session
-from app.infrastructure.database import get_db
+from fastapi.security import OAuth2PasswordRequestForm
 
+from app.presentation import schemas
+from app.core import user_service
+from app.infrastructure.database import get_db
+from app.main import limiter
 
 router = APIRouter()
 
@@ -15,10 +17,9 @@ def endpoint_register_user(new_user:schemas.UserCreate,db:Session = Depends(get_
         return result
     raise HTTPException(status_code=409, detail="User already exists")
 
-
-from fastapi.security import OAuth2PasswordRequestForm
+@limiter.limit("5/minute")
 @router.post('/login')
-def login_user(data : OAuth2PasswordRequestForm = Depends(), db : Session = Depends(get_db)):
+def login_user(request: Request ,data : OAuth2PasswordRequestForm = Depends(), db : Session = Depends(get_db)):
     result =  user_service.login_user(data , db)
     if result is None:
         raise HTTPException(status_code=401, detail="Either Username or Password is wrong !")
